d61861d082ff6533716c2f77d059a8f4995e1f3e
AmanRajput <amanr2599@gmail.com>
2024-04-22 00:17:58 +0530

    banner intro info

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../../Component/Home/Header/Header'
+import Banner from '../../Component/Home/Banner/Banner'
+import Intro from '../../Component/Home/Intro/Intro'
+import Info from '../../Component/Home/Info/Info'
 
 const Home = () => {
   return (
     <>
     <Header/>
+    <Banner/>
+    <Intro/>
+    <Info/>
     </>
   )
 }
